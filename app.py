d3a8f5aa2b09e0a0b7d8a82591957b30a9c3a6e7
SandeepSabat <sandeep.sabat@gmail.com>
2026-01-17 21:24:35 +0530

    changed back end to view list of files for front end

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     CORS(app,resources={r"/*":{"origins":"http://localhost:5173","methods":["POST","OPTIONS","GET"],"allow_headers":["Content-Type","Authorization"]}})
+    app.json.sort_keys = False # this prevents sorting of keys in jsonify and changing the order
   
 
     #Get the absolute path of teh directory containing the current script
@@ -125,6 +126,33 @@ def create_app():
         fileTypeList=['customerlist','htmlformat','emailbounces']
         return render_template('fileUpload.html',fileTypes=fileTypeList)
     
+    def getDirectoryFileDetails(directory_name):
+        filenames = os.listdir(directory_name)
+        # filenames = [{"id":index+1,"filename":value} for index, value in enumerate(filenames_no_key)]
+        full_file_details_list = []
+
+        for index,value  in enumerate(filenames):
+            full_path = os.path.join(directory_name,value)
+            if os.path.isfile(full_path):
+                creation_timestamp = os.path.getctime(full_path)
+                modification_timestamp = os.path.getmtime(full_path)
+
+                # Convert timestamps to readable datetime objects
+                creation_date = datetime.fromtimestamp(creation_timestamp)
+                modification_date = datetime.fromtimestamp(modification_timestamp)
+
+                full_file_details={
+                    "id":index +1,
+                    "filename":value,
+                    'creationtime':creation_date,
+                    'modificationtime':modification_date
+                }
+                full_file_details_list.append(full_file_details)
+                
+        
+        return full_file_details_list
+
+    
     @app.route('/viewFileList',methods=['GET','OPTIONS'])
     def viewFileList():
         if request.method == 'GET' or request.method == 'OPTIONS':
@@ -134,27 +162,26 @@ def create_app():
 
             if filetype == 'customerlist':
                 file_directory = os.path.join(bas_dir,'files','customerlist')
-                filenames_no_key = os.listdir(file_directory)
-                filenames = [{"id":index,"filename":value} for index, value in enumerate(filenames_no_key)]
+                filenames = getDirectoryFileDetails(file_directory)
+                
+                
                 
 
                 
             if filetype == 'htmlformat':
                 file_directory = os.path.join(bas_dir,'files','html')
-                filenames_no_key = os.listdir(file_directory)
-                filenames = [{"id":index,"filename":value} for index, value in enumerate(filenames_no_key)]
+                filenames = getDirectoryFileDetails(file_directory)
                 
             if filetype == 'emailbounces':
                 file_directory = os.path.join(bas_dir,'files','emailbounces')
-                filenames_no_key = os.listdir(file_directory)
-                filenames = [{"id":index,"filename":value} for index, value in enumerate(filenames_no_key)]
+                filenames = getDirectoryFileDetails(file_directory)
                 
             
             return jsonify(filenames)
 
 
     
-    @app.route('/inputFilesList')
+    @app.route('/inputFilesList') # This method to be removed once the front end is fully functional
     def inputFilesList():
         file_directory = os.path.join(bas_dir,'files','customerlist')
 
@@ -162,7 +189,7 @@ def create_app():
 
         return render_template("fileList.html",files=filenames,header='Uploaded Customer Files')
     
-    @app.route('/htmlEmailTemplatesList')
+    @app.route('/htmlEmailTemplatesList') # This method to be removed once the front end is fully functional
     def htmlEmailTemplatesList():
         file_directory = os.path.join(bas_dir,'files','html')
 
@@ -170,7 +197,7 @@ def create_app():
 
         return render_template("fileList.html",files=filenames,header='Uploaded HTML Email Templates')
     
-    @app.route('/emailBouncesList')
+    @app.route('/emailBouncesList') # This method to be removed once the front end is fully functional
     def emailBouncesList():
         file_directory = os.path.join(bas_dir,'files','emailbounces')
 
